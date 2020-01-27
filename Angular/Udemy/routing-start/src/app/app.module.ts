3a6b8dd8d5d6d8eaf639dec6626f396656a08afd
rickgv1 <h.puppo@hotmail.com>
2020-01-27 17:30:57 -0300

    Changed several files related to Authentication

--- Angular/Udemy/routing-start/src/app/app.module.ts
+++ Angular/Udemy/routing-start/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { AuthGuard } from './auth-guard.service';
+import { AuthService } from './auth.service';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
@@ -13,23 +16,7 @@ import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  { path: "", component: HomeComponent },
-  {
-    path: "users", component: UsersComponent,
-    children: [
-      { path: ":id/:name", component: UserComponent }]
-  },
-  {
-    path: "servers", component: ServersComponent,
-    children: [
-      { path: ":id", component: ServerComponent },
-      { path: ":id/edit", component: EditServerComponent }]
-  },
-  { path: 'not-found', component: PageNotFoundComponent },
-  //{ path: 'something', redirectTo: '/something', pathMatch: 'full'}, // 'prefix' (default) or 'full'
-  { path: '**', redirectTo: '/not-found'} // gotta be at the bottom
-];
+
 
 @NgModule({
   declarations: [
@@ -42,8 +29,8 @@ const appRoutes: Routes = [
     ServerComponent,
     PageNotFoundComponent
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
-  providers: [ServersService],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Angular/Udemy/routing-start/src/app/home/home.component.ts
+++ Angular/Udemy/routing-start/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 
@@ -7,14 +8,23 @@ import { Router } from "@angular/router";
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private authService: AuthService) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   onLoadServer(id: number) {
     this.router.navigate(["/servers", id, "edit"], {
       queryParams: { allowEdit: "1" },
       fragment: "loading"
     });
+  }
+
+  onLogin() {
+    this.authService.login();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
