23a44a4b397f5d5eb52b6f5fa2fee3224a0da67c
Henrique <henriquepboueri@gmail.com>
2020-01-31 17:04:18 -0300

    Finished Routing part

--- Angular/Udemy/prj-services-final_/src/app/app-routing.module.ts
+++ Angular/Udemy/prj-services-final_/src/app/app-routing.module.ts
@@ -1,25 +1,25 @@
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipesComponent } from "./recipes/recipes.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-
-
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
-  { path: "", redirectTo: 'recipes', pathMatch: 'full' },
+  { path: "", redirectTo: "recipes", pathMatch: "full" },
   {
-    path: "recipes", component: RecipesComponent,
+    path: "recipes",
+    component: RecipesComponent,
     children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent},
-      { path: ":id", component: RecipeDetailComponent},
-      { path: ':id/edit', component: RecipeEditComponent}
+      { path: "", component: RecipeStartComponent },
+      { path: "new", component: RecipeEditComponent },
+      { path: ":id", component: RecipeDetailComponent },
+      { path: ":id/edit", component: RecipeEditComponent }
     ]
   },
-  { path: "shopping-list", component: ShoppingListComponent }
+  { path: "shopping-list", component: ShoppingListComponent },
+  { path: "**", redirectTo: "recipes"}
   /*{ path: "", component: HomeComponent },
   {
     path: "users",
@@ -63,4 +63,4 @@ const appRoutes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Angular/Udemy/prj-services-final_/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ Angular/Udemy/prj-services-final_/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,32 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
-import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
-import { FnParam } from '@angular/compiler/src/output/output_ast';
+import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { FnParam } from "@angular/compiler/src/output/output_ast";
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.component.html",
+  styleUrls: ["./recipe-detail.component.css"]
 })
 export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService,
-    private route: ActivatedRoute) { }
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
-        /* this.recipe = this.recipeService.getRecipesById(+params['id']); */
-        this.id = +params['id'];
-        this.recipe = this.recipeService.getRecipesById(this.id);
-    }) ;
+      /* this.recipe = this.recipeService.getRecipesById(+params['id']); */
+      this.id = +params["id"];
+      this.recipe = this.recipeService.getRecipesById(this.id);
+    });
   }
 
   onAddToShoppingList() {
-      this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients); 
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    this.router.navigate(["/shopping-list"]);
   }
 
+  onEditRecipe() {
+    this.router.navigate(["edit"], { relativeTo: this.route });
+    //this.router.navigate(["../", this.id, "edit"], { relativeTo: this.route });
+  }
 }
