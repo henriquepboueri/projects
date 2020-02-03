8a14fc527cd240c4b495e3c113c5e02026363d25
Henrique <henriquepboueri@gmail.com>
2020-02-03 12:09:41 -0300

    Finished Routing and Observables

--- Angular/Udemy/obs-02-first-custom-observable/src/app/home/home.component.ts
+++ Angular/Udemy/obs-02-first-custom-observable/src/app/home/home.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
 
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit, OnDestroy {
-
   private firstObsSubscription: Subscription;
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
     this.firstObsSubscription = interval(1000).subscribe(count => {
       console.log(count);
     });
+    interval(1000).subscribe(console.log);
   }
 
   ngOnDestroy(): void {
     this.firstObsSubscription.unsubscribe();
   }
-
 }
