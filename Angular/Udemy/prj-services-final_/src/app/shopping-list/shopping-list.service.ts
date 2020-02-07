1e055850a077240416c3870b634f14700ea7e798
rickgv1 <h.puppo@hotmail.com>
2020-02-07 18:28:09 -0300

    Finished Forms

--- Angular/Udemy/prj-services-final_/src/app/shopping-list/shopping-list.service.ts
+++ Angular/Udemy/prj-services-final_/src/app/shopping-list/shopping-list.service.ts
@@ -1,16 +1,22 @@
-import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class ShoppingListService {
+  startedEditing = new Subject<number>();
   ingredientsChanged = new EventEmitter<Ingredient[]>();
   private ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
+    new Ingredient("Apples", 5),
+    new Ingredient("Tomatoes", 10)
   ];
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient) {
@@ -24,5 +30,15 @@ export class ShoppingListService {
     // }
     this.ingredients.push(...ingredients);
     this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  upgradeIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
