bdcb63ee1851883933c916ebf6331df0f6fb5aa7
Henrique <henriquepboueri@gmail.com>
2021-05-12 09:30:54 -0300

    Incluído espaço para patrocinadores

--- angular/quiz-ufjf/src/app/instrucoes/instrucoes.component.ts
+++ angular/quiz-ufjf/src/app/instrucoes/instrucoes.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-instrucoes',
   templateUrl: './instrucoes.component.html',
-  styleUrls: ['./instrucoes.component.css']
+  styleUrls: ['./instrucoes.component.css'],
 })
 export class InstrucoesComponent implements OnInit {
+  public patrocinadores = [
+    {
+      empresa: 'Bar do Fred',
+      logo: '../../assets/logo_bar_fred.jpg',
+      socials: 'https://www.instagram.com/bardofredgv',
+    },
+    {
+      empresa: 'Versenza Clinic',
+      logo: '../../assets/logo_versenza_clinic.jpg',
+      socials: 'https://www.instagram.com/versenzaclinic',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
