a1f84ca33f02a5200ec23e53ee05ba9415edade3
henriquepboueri <58948107+henriquepboueri@users.noreply.github.com>
2022-04-16 21:24:46 -0300

    sveltestrap

---
+++ svelte/sveltestrap/src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+	let rangeValue = 0;
+</script>
+
+<label for="customRange1" class="form-label">Example range</label>
+<input type="range" class="form-range" id="customRange1" bind:value={rangeValue} />
+
+<p>Value now is {rangeValue}</p>
